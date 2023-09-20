4c2c08b53b735b0c0a3b638a4dd94aa31fb92b37
NikhilRane <nikhilrane61102@gmail.com>
2023-09-20 14:09:00 +0530

    initial

---
+++ app.py
@@ -0,0 +1,171 @@
+from tracker.centroidtracker import CentroidTracker
+from tracker.trackableobject import TrackableObject
+import numpy as np
+import datetime
+import imutils
+import dlib
+import cv2
+import streamlit as st
+import base64
+import tempfile
+
+ARG_confidence = st.sidebar.slider("Confidence (minimum probability to filter weak detections)", 0.0, 1.0, 0.4)
+ARG_skip_frames = st.sidebar.slider("Skip Frames (# of skip frames between detections)", 1, 100, 30)
+threshold = st.sidebar.slider("Threshold (Alert when # of people in one area)", 1, 15, 3)
+
+args = {}
+CLASSES = ["background", "aeroplane", "bicycle", "bird", "boat",
+    "bottle", "bus", "car", "cat", "chair", "cow", "diningtable",
+    "dog", "horse", "motorbike", "person", "pottedplant", "sheep",
+    "sofa", "train", "tvmonitor"]
+
+args["prototxt"] = "detector/MobileNetSSD_deploy.prototxt"
+args["model"] = "detector/MobileNetSSD_deploy.caffemodel"
+net = cv2.dnn.readNetFromCaffe(args["prototxt"], args["model"])
+
+W = None
+H = None
+ct = CentroidTracker(maxDisappeared=40, maxDistance=50)
+trackers = []
+trackableObjects = {}
+
+totalFrames = 0
+totalDown = 0
+totalUp = 0
+total = []
+move_out = []
+move_in = []
+out_time = []
+in_time = []
+
+st.title("People monitoring")
+uploaded_file = st.file_uploader("Choose a video file", type=["mp4", "avi", "mkv", "mov", "264"])
+if uploaded_file is not None:
+    tfile = tempfile.NamedTemporaryFile(delete=False)
+    tfile.write(uploaded_file.read())
+    vs = cv2.VideoCapture(tfile.name)
+    stframe = st.empty()
+
+    while True:
+        frame = vs.read()
+        frame = frame[1]
+        if tfile.name is not None and frame is None:
+            break
+        frame = imutils.resize(frame, width=500)
+        rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+        if W is None or H is None:
+            (H, W) = frame.shape[:2]
+
+        status = "Waiting"
+        rects = []
+
+        if totalFrames % ARG_skip_frames == 0:
+            status = "Detecting"
+            trackers = []
+
+            blob = cv2.dnn.blobFromImage(frame, 0.007843, (W, H), 127.5)
+            net.setInput(blob)
+            detections = net.forward()
+
+            for i in np.arange(0, detections.shape[2]):
+                confidence = detections[0, 0, i, 2]
+
+                if confidence > ARG_confidence:
+                    idx = int(detections[0, 0, i, 1])
+
+                    if CLASSES[idx] != "person":
+                        continue
+
+                    box = detections[0, 0, i, 3:7] * np.array([W, H, W, H])
+                    (startX, startY, endX, endY) = box.astype("int")
+
+                    tracker = dlib.correlation_tracker()
+                    rect = dlib.rectangle(startX, startY, endX, endY)
+                    tracker.start_track(rgb, rect)
+
+                    trackers.append(tracker)
+
+        else:
+            for tracker in trackers:
+                status = "Tracking"
+
+                tracker.update(rgb)
+                pos = tracker.get_position()
+
+                startX = int(pos.left())
+                startY = int(pos.top())
+                endX = int(pos.right())
+                endY = int(pos.bottom())
+
+                rects.append((startX, startY, endX, endY))
+
+        cv2.line(frame, (0, H // 2), (W, H // 2), (0, 0, 0), 3)
+        cv2.putText(frame, "-Prediction border - Entrance-", (10, H - ((i * 20) + 200)),
+            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 1)
+
+        objects = ct.update(rects)
+
+        for (objectID, centroid) in objects.items():
+            to = trackableObjects.get(objectID, None)
+
+            if to is None:
+                to = TrackableObject(objectID, centroid)
+            else:
+                y = [c[1] for c in to.centroids]
+                direction = centroid[1] - np.mean(y)
+                to.centroids.append(centroid)
+
+                if not to.counted:
+                    if direction < 0 and centroid[1] < H // 2:
+                        totalUp += 1
+                        date_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M")
+                        move_out.append(totalUp)
+                        out_time.append(date_time)
+                        to.counted = True
+                    elif direction > 0 and centroid[1] > H // 2:
+                        totalDown += 1
+                        date_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M")
+                        move_in.append(totalDown)
+                        in_time.append(date_time)
+
+                        if sum(total) >= threshold:
+                            cv2.putText(frame, "-ALERT: People limit exceeded-", (10, frame.shape[0] - 80),
+                                cv2.FONT_HERSHEY_COMPLEX, 0.5, (0, 0, 255), 2)
+                        to.counted = True
+                        total = []
+                        total.append(len(move_in) - len(move_out))
+
+            trackableObjects[objectID] = to
+
+            text = "ID {}".format(objectID)
+            cv2.putText(frame, text, (centroid[0] - 10, centroid[1] - 10),
+                cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+            cv2.circle(frame, (centroid[0], centroid[1]), 4, (255, 255, 255), -1)
+
+        info_status = [
+            ("Exit", totalUp),
+            ("Enter", totalDown),
+            ("Status", status),
+        ]
+
+        info_total = [
+            ("Total people inside", ', '.join(map(str, total))),
+        ]
+
+        for (i, (k, v)) in enumerate(info_status):
+            text = "{}: {}".format(k, v)
+            cv2.putText(frame, text, (10, H - ((i * 20) + 20)), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 0, 0), 2)
+
+        for (i, (k, v)) in enumerate(info_total):
+            text = "{}: {}".format(k, v)
+            cv2.putText(frame, text, (265, H - ((i * 20) + 60)), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 255), 2)
+
+        _, buffer = cv2.imencode('.jpg', frame)
+        frame_base64 = base64.b64encode(buffer).decode()
+
+        stframe.markdown(f'<img src="data:image/jpeg;base64,{frame_base64}"/>', unsafe_allow_html=True)
+        totalFrames += 1
+
+    cv2.destroyAllWindows()
+
